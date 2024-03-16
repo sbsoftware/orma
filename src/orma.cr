bf58723ac73b10b06876c6459dbd9624e5db0de8
Stefan Bilharz <stefan@sbsoftware.de>
2024-03-16 10:52:32 +0100

    ORM-11 Add experimental continuous migration feature

--- src/orma.cr
+++ src/orma.cr
@@ -1,1 +1,8 @@
 require "./orma/record"
+
+if ENV.fetch("ORMA_CONTINUOUS_MIGRATION", "").in?(["1", "true"])
+  {% for orm_class in Orma::Record.all_subclasses %}
+    puts "Creating table for #{{{orm_class.id}}}"
+    {{orm_class.id}}.ensure_table_exists!
+  {% end %}
+end
