e4c111820327fd243a564af976eca2de96ce9058
Stefan Bilharz <stefan@sbsoftware.de>
2024-03-16 22:30:00 +0100

    ORM-7 Don't consider abstract model classes for continuous migration

--- src/orma.cr
+++ src/orma.cr
@@ -2,7 +2,9 @@ require "./orma/record"
 
 if ENV.fetch("ORMA_CONTINUOUS_MIGRATION", "").in?(["1", "true"])
   {% for orm_class in Orma::Record.all_subclasses %}
-    puts "Creating table for #{{{orm_class.id}}}"
-    {{orm_class.id}}.ensure_table_exists!
+    {% if !orm_class.abstract? %}
+      puts "Creating table for #{{{orm_class.id}}}"
+      {{orm_class.id}}.ensure_table_exists!
+    {% end %}
   {% end %}
 end
