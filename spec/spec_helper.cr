4844241f44b81eb8b83e17cdbe0fbcb515a40bf0
Stefan Bilharz <stefan@sbsoftware.de>
2025-07-03 21:55:25 +0200

    ORM-41 Introduce TestRecord base class

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,3 +1,22 @@
 require "spec"
 require "../src/orma"
 require "./fake_db"
+require "sqlite3"
+
+abstract class TestRecord < Orma::Record
+  DB_PATH = "./test.db"
+
+  id_column id : Int64
+
+  def self.db_connection_string
+    "sqlite3://#{DB_PATH}"
+  end
+
+  macro inherited
+    self.continuous_migration!
+  end
+end
+
+if File.exists?(TestRecord::DB_PATH)
+  File.delete(TestRecord::DB_PATH)
+end
